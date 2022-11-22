c3c87e2b6069c4367e01e69b08c44ae852a53806
dougreichard <doug.reichard+gitlab@gmail.com>
2022-11-21 23:05:01 -0500

    console example

--- script.py
+++ script.py
@@ -6,11 +6,14 @@ from sbs_utils.mast.maststoryscheduler import StoryPage
 
 
 class MyStoryPage(StoryPage):
-    #story_file = "story.mast"
+    story_file = "story.mast"
     #story_file = "examples/credits.mast"
-    story_file = "examples/bar.mast"
+    #story_file = "examples/bar.mast"
     #story_file = "examples/joke.mast"
+    #story_file = "examples/consoles.mast"
 
 Gui.server_start_page_class(MyStoryPage)
 Gui.client_start_page_class(MyStoryPage)
+
+
 
