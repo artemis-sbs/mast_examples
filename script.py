f932a3000f44d4b5cf256bca9f35937d01dd1176
dougreichard <doug.reichard+gitlab@gmail.com>
2022-11-28 16:29:06 -0500

    changes to support new style system

--- script.py
+++ script.py
@@ -3,7 +3,24 @@ from  sbs_utils.handlerhooks import *
 
 from sbs_utils.gui import Gui
 from sbs_utils.mast.maststoryscheduler import StoryPage
+from sbs_utils.mast.mast import Mast
+import sbs
 
+@Mast.make_global
+class MyClass:
+    def func():
+        print("class member")
+
+@Mast.make_global
+def create_map(mast_scheduler):
+    sim = mast_scheduler.sim
+    artemis = mast_scheduler.PlayerShip().spawn(sim, 0, 0, 0,"Artemis", "tsn", "Battle Cruiser").py_object
+    hera = mast_scheduler.PlayerShip().spawn(sim, 0, 0, 0,"Hera", "tsn", "Battle Cruiser").py_object
+    atlas = mast_scheduler.PlayerShip().spawn(sim, 0, 0, 0,"Atlas", "tsn", "Battle Cruiser").py_object
+    sbs.assign_client_to_ship(0, artemis.id)
+    Mast.make_global_var("artemis", artemis)
+    Mast.make_global_var("hera", hera)
+    Mast.make_global_var("atlas", atlas)
 
 class MyStoryPage(StoryPage):
     story_file = "story.mast"
